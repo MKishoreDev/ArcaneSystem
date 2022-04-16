93208747488b70c60e0e06d289d7a8c52e1fc047
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-04-16 13:23:22 +0530

    Update pmstuffs.py

--- KawaiiXRobot/plugins/pmstuffs.py
+++ KawaiiXRobot/plugins/pmstuffs.py
@@ -1,10 +1,10 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Cringe import bot
+from KawaiiXRobot import bot
 
-cringeguys = """
-<b>Hey</b> {} <b>Welcome To CringeXSystem</b> 
+Kawaii = """
+<b>Hey</b> {} <b>Welcome To Kawaii X Robot</b> 
 <b>An Advanced System To Make Telegram Safe</b>
 <b>Checkout The</b> `/help`
 """
@@ -13,15 +13,15 @@ hehe = "https://telegra.ph/file/51f6b8109b0731db41394.mp4"
 @bot.on_message(filters.command(["start"], ['/', ".", "?"]))
 async def start(client, message):
     buttons = [[
-        InlineKeyboardButton("Dev", url="https://t.me/Aasf_Cyberking"),
+        InlineKeyboardButton("Dev", url="https://t.me/HMF_OWNER_1"),
     ]]
-    await message.reply_video(hehe, caption=cringeguys.format(message.from_user.mention),
+    await message.reply_video(hehe, caption=Kawaii.format(message.from_user.mention),
                              reply_markup=InlineKeyboardMarkup(buttons))
 
 @bot.on_message(filters.command(["help"], ['/', ".", "?"]))
 async def help(client, message):
     buttons = [[
-        InlineKeyboardButton("Dev", url="https://t.me/Aasf_Cyberking"),
+        InlineKeyboardButton("Dev", url="https://t.me/HMF_OWNER_1"),
     ]]
     await message.reply_text(""" **COMMANDS** :  
 
@@ -37,11 +37,11 @@ async def help(client, message):
 /setrole - Roles Name
 /report - Report Message
 
-**Clan Of Cringe X System Commands** :
+**Clan Of Kawaii X System Commands** :
 
 /scan userid reason or reply to user - Ban User
 /unscan userid - Unban User
 /gscan Username - All Group Admin Ban
 /ungscan Username - All Group Admin Unban
-/logs - Check Cringe X system logs""",
+/logs - Check Kawaii X system logs""",
                              reply_markup=InlineKeyboardMarkup(buttons))
