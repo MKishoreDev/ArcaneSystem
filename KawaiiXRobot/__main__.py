486fcb0b98bdd3d3840d18f169ab338cb11778d5
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-04-16 12:57:05 +0530

    Add files via upload

---
+++ KawaiiXRobot/__main__.py
@@ -0,0 +1,13 @@
+from Cringe import bot
+import logging
+
+logging.basicConfig(
+    filename="logs.txt",
+    level=logging.INFO,
+    filemode="a",
+    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+if __name__ == "__main__":
+    bot.run()
