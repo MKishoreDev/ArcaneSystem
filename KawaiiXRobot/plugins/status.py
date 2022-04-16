77608ab7365d804ee3af58bdc34ea17e151ebf5e
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-04-16 13:28:27 +0530

    Update status.py

--- KawaiiXRobot/plugins/status.py
+++ KawaiiXRobot/plugins/status.py
@@ -1,23 +1,23 @@
 import asyncio
 import time
 
-from Cringe import bot, db, DEVS, CRINGE_GUYS, CRINGE_KID
+from KawaiiXRobot import bot, db, DEVS, KAWAII_A_RANK, KAWAII_B_RANK
 from pyrogram import filters
 from pyrogram.types import Message
 
 on_string = """
-╒═══「 Cringe X System • Server Core 」
+╒═══「 Kawaii X System • Server Core 」
 ➖➖➖➖➖➖➖➖➖
 ➣ NAME: {name}
 ➢ RANK: {status}
 ➖➖➖➖➖➖➖➖➖
-╘══「 You are a Cringe authorized user! 」
+╘══「 You are a Kawaii authorized user! 」
 """
 
-CIRINGE = "https://telegra.ph/file/c5a59655aede70ac6135c.mp4"
+KAWAII = "https://telegra.ph/file/c5a59655aede70ac6135c.mp4"
 @bot.on_message(filters.command("status", ['/', ".", "?"]))
 async def status(bot, m: Message):
-    msg = await m.reply("Conecting to Cringe X System System Core.")
+    msg = await m.reply("Conecting to Kawaii X System System Core.")
     time.sleep(1)
     await msg.edit("Initialising ✦✧✧✧✧✧")
     time.sleep(1)
@@ -34,16 +34,16 @@ async def status(bot, m: Message):
     await msg.edit("✪VᴇRɪFɪᴇD✪")
     time.sleep(2)
     if m.from_user.id in DEVS:
-        status = "**God Of Cringe**"
+        status = "**God Of Kawaii**"
 
-    if m.from_user.id in CRINGE_GUYS:
-        status = "**Guys Of Cringe**"
+    if m.from_user.id in KAWAII_A_RANK:
+        status = "**A Rank of Kawaii**"
 
-    if m.from_user.id in CRINGE_KID:
-        status = "**Kid Of Cringe**"
+    if m.from_user.id in KAWAII_B_RANK:
+        status = "**B Rank of Kawaii**"
 
     elif db.get_role(m.from_user.id)['status'] != True:
-        status = "Human Of Cringe"
+        status = "Human Of Kawaii"
 
     elif db.get_role(m.from_user.id)['status'] == True:
         status = db.get_role(m.from_user.id)['role']
@@ -51,13 +51,13 @@ async def status(bot, m: Message):
     else:
         status = "Human"
     time.sleep(1)
-    await msg.edit(on_string.format(status=status, name=m.from_user.mention, file=CIRINGE))
+    await msg.edit(on_string.format(status=status, name=m.from_user.mention, file=KAWAII))
 
-@bot.on_message(filters.command("cringe"))
+@bot.on_message(filters.command("Kawaii"))
 def setstatus(_, m: Message):
     role = m.text.replace(m.text.split(" ")[0], "")
     if role != "":
         db.add_role(m.from_user.id, role)
         m.reply("`Done!`")
     else:
-        m.reply("Usage : `/cringe cringe role`")
+        m.reply("Usage : `/Kawaii role`")
