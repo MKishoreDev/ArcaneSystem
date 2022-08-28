7b2c39bbe2a1eb2d29f45b6a1d2f7eed1c05a7fb
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-28 13:47:06 +0530

    Create pinfo.py

---
+++ KawaiiXRobot/plugins/pinfo.py
@@ -0,0 +1,52 @@
+import asyncio
+from KawaiiXRobot import bot, db
+from pyrogram import filters
+from config import DEVS, Inspector, Enforcer 
+from pyrogram.types import Message
+
+@bot.on_message(filters.command("pinfo", ['/', ".", "?"]))
+async def status(bot, m: Message):
+
+    if m.from_user.id in DEVS:
+        status = "**God Of Cringe**"
+
+    elif db.get_role(m.from_user.id)['status'] != True:
+        status = "Human"
+
+    elif db.get_role(m.from_user.id)['status'] == True:
+        status = db.get_role(m.from_user.id)['role']
+
+    else:
+        status = "civilian"
+
+    if m.from_user.id in DEVS:
+        text = f"""
+╒═══「 Cringe X System  」
+➖➖➖➖➖➖➖➖➖
+➣ NAME: {name}
+➢ RANK: {status}
+➖➖➖➖➖➖➖➖➖
+╘══「 You are a Kawaii authorized user! 」
+"""
+
+        await m.reply_photo("https://wallpaperaccess.com/full/6766185.jpg",
+                            caption=text,
+                            parse_mode="markdown")
+
+    else:
+        text = f"""
+**Welcome {m.from_user.first_name}**,
+**Status** : **{status}**
+"""
+
+        await m.reply(text, parse_mode="markdown")
+
+
+@bot.on_message(filters.command("setrole"))
+def setstatus(_, m: Message):
+    role = m.text.replace(m.text.split(" ")[0], "")
+    if not role == "":
+        db.add_role(m.from_user.id, role)
+        m.reply("Done!")
+    else:
+        m.reply("Usage : /setrole role")
