1cb2d67c042acec8abb967361248eef20a9bd3c4
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-28 15:25:18 +0530

    Update dev.py

--- KawaiiXRobot/plugins/dev.py
+++ KawaiiXRobot/plugins/dev.py
@@ -142,5 +142,5 @@ async def ping(_, m):
     end_time = time.time()
     ping_time = round((end_time - start_time) * 1000, 3)
     uptime = get_readable_time((time.time() - StartTime))
-    await text.edit_text(f"**🏓 PONG!!:** `{ping_time} ms`\n**🆙 UPTIME:** `{uptime}`", parse_mode='markdown')
+    await text.edit_text(f"**🏓 PONG!!:** `{ping_time} ms`\n**🆙 UPTIME:** `{uptime}`")
 
