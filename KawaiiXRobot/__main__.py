4e625e0cfe6d8f2c9b110b8694f1b340e3655b6e
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-28 12:15:53 +0530

    Update __main__.py

--- KawaiiXRobot/__main__.py
+++ KawaiiXRobot/__main__.py
@@ -1,5 +1,27 @@
 from KawaiiXRobot import bot
 import logging
+import glob
+import importlib
+import sys
+from pathlib import Path
+
+def load_plugins(plugin_name):
+    path = Path(f"KawaiiXRobot/plugins/{plugin_name}.py")
+    name = "KawaiiXRobot.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["KawaiiXRobot.plugins." + plugin_name] = load
+    print("Imported --> " + plugin_name)
+
+path = "KawaiiXRobot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        thepath = Path(a.name)
+        plugin_name = thepath.stem
+        load_plugins(plugin_name.replace(".py", ""))
 
 logging.basicConfig(
     filename="logs.txt",
