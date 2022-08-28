5152fc5b9c8864742446777ef4520f0c636133a3
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-28 11:30:08 +0530

    1 min testing

--- KawaiiXRobot/__init__.py
+++ KawaiiXRobot/__init__.py
@@ -14,15 +14,8 @@ KAWAII_CHANNEL = os.environ.get("KAWAII_CHANNEL", None)
 DEVS = os.environ.get("DEVS", None)
 Inspector = os.environ.get("Inspector", None)
 Enforcer = os.environ.get("Enforcer", None)
-DEVS = [
- 5175767264, 1989750989, 1491497760
-]
-Inspector = [
- 1057561033, 5163504846, 1491497760
-]
-Enforcer = [
- 1891633746, 5163504846, 1491497760
-]
+
+
 bot = Client("KawaiiXRobot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 db = DATABASE(MONGO_URI)
 mongo_client = MongoClient("mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority")
