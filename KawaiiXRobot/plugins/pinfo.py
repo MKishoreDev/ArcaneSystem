f176bb49dde33768ece66263957865a31e206e73
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-28 14:22:43 +0530

    Update pinfo.py

--- KawaiiXRobot/plugins/pinfo.py
+++ KawaiiXRobot/plugins/pinfo.py
@@ -25,7 +25,7 @@ async def status(bot, m: Message):
     else:
         status = "civilian"
 
-if m.from_user.id in DEVS, Inspector, Enforcer:
+    if m.from_user.id in DEVS, Inspector, Enforcer:
         dev_text = f"""
 ╒═══「 Cringe X System  」
 ➖➖➖➖➖➖➖➖➖
