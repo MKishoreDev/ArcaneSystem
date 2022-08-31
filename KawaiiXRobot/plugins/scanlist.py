47680babb2eb060c7baad1340f7bfe161c41abc1
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-31 15:37:22 +0530

    Update scanlist.py

--- KawaiiXRobot/plugins/scanlist.py
+++ KawaiiXRobot/plugins/scanlist.py
@@ -4,6 +4,9 @@ from KawaiiXRobot.utils.dbfunctions import get_gbans_count
 
 @bot.on_message(filters.command(["scanlist"], ['/', ".", "?"]))
 async def list(client, message):
+ try:
     gbans = await get_gbans_count()
     hehe = "https://telegra.ph/file/65239f3043ca5161617df.mp4"
     await message.reply_video(hehe, caption="`{}` **Globally Scanned/Banned Users**".format(get_gbans_count))
+ except Exception:
+    pass
