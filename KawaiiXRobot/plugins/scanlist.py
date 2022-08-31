ac29ea73701ff391b40abce6cef9ba25b1d51cdd
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-31 15:33:28 +0530

    Create scanlist.py

--- KawaiiXRobot/plugins/scanlist.py
+++ KawaiiXRobot/plugins/scanlist.py
@@ -5,5 +5,4 @@ from KawaiiXRobot.utils.dbfunctions import get_gbans_count
 @bot.on_message(filters.command(["scanlist"], ['/', ".", "?"]))
 async def list(client, message):
     gbans = await get_gbans_count()
-    hehe = "https://telegra.ph/file/65239f3043ca5161617df.mp4"
-    await message.reply_video(hehe, caption="`{}` **Globally Scanned/Banned Users**".format(get_gbans_count))
+    await message.reply_text(text="`{}` **Globally Scanned/Banned Users**".format(get_gbans_count))
