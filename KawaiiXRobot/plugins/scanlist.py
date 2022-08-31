0566879f7057fe8f2feb94734413cf5b161c6d89
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-31 15:26:47 +0530

    Create scanlist.py

---
+++ KawaiiXRobot/plugins/scanlist.py
@@ -0,0 +1,8 @@
+from pyrogram import filters, Client
+from KawaiiXRobot import bot
+from KawaiiXRobot.utils.dbfunctions import get_gbans_count
+
+@bot.on_message(filters.command(["scanlist"], ['/', ".", "?"]))
+async def list(client, message):
+    gbans = await get_gbans_count()
+    await message.reply_text(hehe, caption="`{}` **Globally Scanned/Banned Users**".format(get_gbans_count)
