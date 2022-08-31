7425ac9209e3c43b538c4b820059c7a21dee7a67
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-31 22:48:12 +0530

    Update start.py

--- KawaiiXRobot/plugins/start.py
+++ KawaiiXRobot/plugins/start.py
@@ -49,14 +49,14 @@ help_buttons = [
         ]
 
 
-@bot.on_message(invaded_cmd("start"))
+@bot.on_message(filters.command(["start"], ['/', ".", "?"]))
 async def start(client, message):
              await message.reply_video(HMF_VID,
                 caption=PM_START_TEXT.format(message.from_user.mention),                   
                 reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.command("help"))
-async def start(client, message):
+async def help(client, message):
              await message.reply_video(HMF_VID,
                 caption=HELP.format(message.from_user.mention),                   
                 reply_markup=InlineKeyboardMarkup(help_buttons))
