5fe6e97db36798514131738914bd0df725731c0a
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-31 15:13:23 +0530

    Update unscan.py

--- KawaiiXRobot/plugins/unscan.py
+++ KawaiiXRobot/plugins/unscan.py
@@ -19,10 +19,10 @@ async def revive(_, message):
           elif len(message.command) <2:
                 return await message.reply("give a user ID or name")
           elif not message.reply_to_message:
-          user_id_text = message.text.split(None, 1)[1]
-          user = await bot.get_users(user_id_text)
-          await x.remove_gban_user(user.id)
-          await message.reply(f"Kawai User: {message.from_user.id}\n target user: {user.id}\n **UNBANNED**")
+                    user_id_text = message.text.split(None, 1)[1]
+                    user = await bot.get_users(user_id_text)
+                    await x.remove_gban_user(user.id)
+                    await message.reply(f"Kawai User: {message.from_user.id}\n target user: {user.id}\n **UNBANNED**")
           else:
                await message.reply(f"{user.mention} he's not banned")
                if message.from_user.id in OWO and message.reply_to_message:
