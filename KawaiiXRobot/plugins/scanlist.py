5faad82b517bce4a47afd31a96a9d7195351cb23
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-31 15:32:49 +0530

    Update scanlist.py

--- KawaiiXRobot/plugins/scanlist.py
+++ KawaiiXRobot/plugins/scanlist.py
@@ -5,4 +5,4 @@ from KawaiiXRobot.utils.dbfunctions import get_gbans_count
 @bot.on_message(filters.command(["scanlist"], ['/', ".", "?"]))
 async def list(client, message):
     gbans = await get_gbans_count()
-    await message.reply_text(hehe, caption="`{}` **Globally Scanned/Banned Users**".format(get_gbans_count))
+    await message.reply_text(message.chat.id, caption="`{}` **Globally Scanned/Banned Users**".format(get_gbans_count))
