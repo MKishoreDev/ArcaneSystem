c950e72bbceac1500e0d1ffb9890ed52614a35cf
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-31 15:17:28 +0530

    Update unscan.py

--- KawaiiXRobot/plugins/unscan.py
+++ KawaiiXRobot/plugins/unscan.py
@@ -14,20 +14,21 @@ OWO = DEVS + Inspector
 
 @bot.on_message(filters.command("revert"))
 async def revive(_, message):
-          if message.from_user.id not in OWO:
-                return await message("Only OWO user can use")
-          elif len(message.command) <2:
-                return await message.reply("give a user ID or name")
-          elif not message.reply_to_message:
-                    user_id_text = message.text.split(None, 1)[1]
-                    user = await bot.get_users(user_id_text)
-                    await x.remove_gban_user(user.id)
-                    await message.reply(f"Kawai User: {message.from_user.id}\n target user: {user.id}\n **UNBANNED**")
-          else:
-               await message.reply(f"{user.mention} he's not banned")
+          await message.reply(f"{user.mention} he's not banned")
                if message.from_user.id in OWO and message.reply_to_message:
                        await x.remove_gban_user(message.reply_to_message.from_user.id)
-                       await message.reply(f"Kawai User: {message.from_user.id}\n target user: {message.reply_to_message.from_user.id}\n **UNBANNED**")
+                       await message.reply(f"Kawai User: {message.from_user.id}\n target user: {message.reply_to_message.from_user.id}\n **UNBANNED**")  
           
+          else:
+                if message.from_user.id not in OWO:
+                      return await message("Only OWO user can use")
+                elif len(message.command) <2:
+                      return await message.reply("give a user ID or name")
+                elif not message.reply_to_message:
+                     user_id_text = message.text.split(None, 1)[1]
+                     user = await bot.get_users(user_id_text)
+                     await x.remove_gban_user(user.id)
+                     await message.reply(f"Kawai User: {message.from_user.id}\n target user: {user.id}\n **UNBANNED**")
+               
 
                
