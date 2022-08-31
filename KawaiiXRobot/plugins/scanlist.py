889c8ed805b029246d1180bf12e71a18b691b07c
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-31 19:07:35 +0530

    Update scanlist.py

--- KawaiiXRobot/plugins/scanlist.py
+++ KawaiiXRobot/plugins/scanlist.py
@@ -6,14 +6,18 @@ from KawaiiXRobot.utils.dbfunctions import gbansdb
 async def list(client, message):
    total_num = len([i async for i in gbansdb.find({"user_id": {"$gt": 0}})])
    total_id = ([i['user_id'] async for i in gbansdb.find({"user_id": {"$gt": 0}})])
+   n = 0
    reply = f"`{total_num}` **Globally Scanned/Banned Users\n**"
    for x in total_id:
        try: 
            user = await bot.get_users(int(x))
            mention = "[" + user.first_name + "](tg://user?id=" + str(user.id) + ")"
-           reply = f"`{total_num}` **Globally Scanned/Banned Users\n**"
-           reply += f"• {mention}\n"
+           n += 1
+       except Exception:
+           continue
+           reply += f"{n}. {mention}\n"
+       if reply == "":
+           return await message.reply_texf(text="`No Scanned/Banned Found.`")
+       else:
            hehe = "https://telegra.ph/file/65239f3043ca5161617df.mp4"
            await message.reply_video(hehe, caption=reply)
-       except Exception:
-           pass
