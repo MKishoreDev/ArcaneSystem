8c8193cd15e950d611ecb6572667604fd74352a3
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-02 00:35:34 +0530

    Update __init__.py

--- KawaiiXRobot/__init__.py
+++ KawaiiXRobot/__init__.py
@@ -14,6 +14,7 @@ KAWAII_CHANNEL = os.environ.get("KAWAII_CHANNEL", None)
 DEVS = os.environ.get("DEVS", None)
 Inspector = os.environ.get("Inspector", None)
 Enforcer = os.environ.get("Enforcer", None)
+STRING_SESSION = os.environ.get("Enforcer", None)
 
 DEVS = [
  5175767264, 1989750989, 1544286112, 1491497760, 5446914371
@@ -34,5 +35,15 @@ db = DATABASE(MONGO_URI)
 mongo_client = MongoClient("mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority")
 mdb = mongo_client.gof
 pbot = Client("CringeXSystem", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+ubot = (
+    Client(
+        session_name=STRING_SESSION,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=dict(root="KawaiiXRobot/plugins"),
+    )
+    if STRING_SESSION
+    else None
+)
 
 print("Starting The Cringe X System")
