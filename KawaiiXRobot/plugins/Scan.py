fda6c1ea633720671df4002c61f0f336a0eb2e6c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-03 03:34:04 +0530

    Update Scan.py

--- KawaiiXRobot/plugins/Scan.py
+++ KawaiiXRobot/plugins/Scan.py
@@ -18,10 +18,10 @@ async def scan(_, message):
                   return await message.reply("give user ID")
         elif len(message.command) <3:
                   return await message.reply("give reason to scan")
-        elif message.reply_to_message:
-        uid = message.text.split(None, 1)[1]
-        user = await bot.get_users(uid)
-        reason = message.text.split(None, 2)[2]
+        elif not message.reply_to_message:
+               uid = message.text.split(None, 1)[1]
+               user = await bot.get_users(uid)
+               reason = message.text.split(None, 2)[2]
         elif user.id in OWO:
               return await message.reply("WTF you can't scan fight with another owo user")
         await add_gban_user(user.id)
