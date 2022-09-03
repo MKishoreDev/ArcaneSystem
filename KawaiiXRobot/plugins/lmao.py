2b0a7968781b7b8040a9a49dda51052be1769585
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-03 17:22:24 +0530

    Update lmao.py

--- KawaiiXRobot/plugins/lmao.py
+++ KawaiiXRobot/plugins/lmao.py
@@ -6,11 +6,15 @@ OWO = DEVS + Inspector
 
 @bot.on_message(filters.command("fuck") & filters.user(OWO) & ~filters.forwarded)
 async def _(_, message):
-    reason = len(message.command) <3
-    if message.reply_to_message:
+ if len(message.command) <2:
+     return await message.reply("give user ID")
+ elif len(message.command) <3:
+    return await message.reply("give reason to scan")
+    reason = message.text.split(None, 2)[2]
+  if message.reply_to_message:
          r_from_id = message.reply_to_message.from_user.id
     else:
-         r_from_id = len(message.command) <2
+         r_from_id = message.text.split(None, 1)[1]
          await bot.send_message(
             KAWAII_LOGS,
             "/gban [user](tg://user?id={}) {} By {}".format(r_from_id, reason, message.from_user.id)
