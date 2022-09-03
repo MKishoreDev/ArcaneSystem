d93ea5f5676c6c48774b01bd686834e1e74129bb
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-03 22:41:17 +0530

    Update Scan.py
    Done

--- KawaiiXRobot/plugins/Scan.py
+++ KawaiiXRobot/plugins/Scan.py
@@ -10,35 +10,22 @@ from KawaiiXRobot.utils.dbfunctions import (
 
 OWO = DEVS + Inspector
 
-@bot.on_message(filters.command("scan"))
-async def scan(_, message):
-        if not message.from_user.id in OWO and not message.reply_to_message:
-               return await message.reply("Only owo users can scan")
-        elif len(message.command) <2:
-                  return await message.reply("give user ID")
-        elif len(message.command) <3:
-                 return await message.reply("give reason to scan")
-                 user_id_text = message.text.split(None, 1)[1]
-                 user = await bot.get_users(user_id_text)
-                 reason = message.text.split(None, 2)[2]
-                 await add_gban_user(user.id)
-                 await bot.send_message(message.chat.id, "this means the users is scanned hacker add here text")
-        else:
-               if len(message.command) <2:
-                       await message.reply("give reason boi")
-               reason = message.text.split(None, 1)[1]
-               user_id = message.reply_to_message.from_user.id
-               info = await bot.get_users(user_id)
-               await add_gban_user(user.id)
-               await bot.send_message(message.chat.id,
-                    f"""
-#BANNED
-**USER**: [{user}](tg://user?id={info.id})
-**REASON**: {reason}
-**ENFORCER**: [{enforcer}](tg://user?id={message.from_user.id})
-**CHAT_ID** : {message.chat.id}
-""")
-               
-                    
-            
+@bot.on_message(filters.command("revert"))
+async def revive(_, message):
+          
+          if message.from_user.id in OWO and message.reply_to_message:
+                       await add_gban_user(message.reply_to_message.from_user.id)
+                       await message.reply(f"Kawai User: {message.from_user.id}\n target user: {message.reply_to_message.from_user.id}\n **BANNED**")  
+          
+          else:
+                if message.from_user.id not in OWO:
+                      return await message("Only OWO user can use")
+                elif len(message.command) <2:
+                      return await message.reply("give a user ID or name")
+                elif not message.reply_to_message:
+                     user_id_text = message.text.split(None, 1)[1]
+                     user = await bot.get_users(user_id_text)
+                     await add_gban_user(user.id)
+                     await message.reply(f"Kawai User: {message.from_user.id}\n target user: {user.id}\n **BANNED**")
+             
 
