53d7e4863e39bb01eb1d5a68510052a437b7ee08
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-03 10:38:54 +0530

    ` Testing `

--- KawaiiXRobot/plugins/skan.py
+++ KawaiiXRobot/plugins/skan.py
@@ -10,7 +10,7 @@ from KawaiiXRobot.utils.dbfunctions import (
 
 OWO = DEVS + Inspector
 
-@ubot.on_message(filters.command("skan", prefixes=["/", ".", "?", "-"]))
+@bot.on_message(filters.command("skan", prefixes=["/", ".", "?", "-"]))
 async def uban(Client, m: Message):
      if not m.from_user.id in OWO:
         await m.reply_text("Only The Cringe Inspectors Can Use Me")
