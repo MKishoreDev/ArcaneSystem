6af908ffe49d48f436a611a346a0041e0a5c8e81
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-03 16:10:19 +0530

    Create lmao.py

---
+++ KawaiiXRobot/plugins/lmao.py
@@ -0,0 +1,39 @@
+import asyncio
+from KawaiiXRobot import KAWAII_LOGS, bot, DEVS, Inspector
+from pyrogram import filters
+
+OWO = DEVS + Inspector
+
+@bot.on_message(filters.command("scan") & filters.user(OWO) & ~filters.forwarded)
+async def _(_, message):
+    reason = len(message.command) <3:
+    if message.reply_to_message:
+         r_from_id = message.reply_to_message.from_user.id
+    else:
+         r_from_id = len(message.command) <2:
+         await bot.send_message(
+            KAWAII_LOGS,
+            "/gban [user](tg://user?id={}) {} By {}".format(r_from_id, reason, message.from_user.id)
+         )
+    await message.delete()
+    await message.reply_text("**Gbanning...**")
+    asyncio.sleep(3.5)
+    await message.reply_text("**Gbanned Succesfully**")
+    await message.delete()
+
+
+@bot.on_message(filters.command("revive") & filters.forwarded)
+async def _(_, message):
+    if message.reply_to_message:
+         r_from_id = message.reply_to_message.from_user.id
+    else:
+         r_from_id = len(message.command) <2:
+         await bot.send_message(
+            KAWAII_LOGS,
+            "/ungban [user](tg://user?id={}) By {}".format(r_from_id, message.from_user.id)
+         )
+    await message.delete()
+    await message.reply_text("**UnGbanning...**")
+    asyncio.sleep(3.5)
+    await message.reply_text("**UnGbanned Succesfully**")
+    await message.delete()
