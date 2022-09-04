800f95b9d900500045ee7efa845edbf51272dc50
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-04 21:56:36 +0530

    ` Sudo Changed into Ins `

---
+++ KawaiiXRobot/utils/ins_db.py
@@ -0,0 +1,33 @@
+from .db import Db
+
+
+class DB(Db):
+    async def get_ins(self, chat_id: int):
+        return [
+            row[1]
+            for row in await self.db.fetch_all(
+                "select * from ins_db where chat_id = :chat_id", {"chat_id": chat_id}
+            )
+        ]
+
+    async def add_ins(self, chat_id: int, user_id: int):
+        if user_id in await self.get_ins(chat_id):
+            return "already_become_ins"
+        await self.db.execute(
+            "insert into ins_db values (:chat_id, :user_id)",
+            {"chat_id": chat_id, "user_id": user_id},
+        )
+        return "added_ins"
+
+    async def del_ins(self, chat_id: int, user_id: int):
+        if user_id not in await self.get_ins(chat_id):
+            return "already_deleted_ins"
+        await self.db.execute(
+            "delete from ins_db where chat_id = :chat_id and user_id = :user_id",
+            {"chat_id": chat_id, "user_id": user_id},
+        )
+        return "deleted_ins"
+
+
+ins_db = insDB()
+
