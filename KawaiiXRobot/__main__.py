391e4585ccad64cd6c16823310c241dad7c30589
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-04 09:32:28 +0530

    Update __main__.py

--- KawaiiXRobot/__main__.py
+++ KawaiiXRobot/__main__.py
@@ -6,7 +6,9 @@ import importlib
 import sys
 from pathlib import Path
 
-def load_plugins(plugin_name, files):
+def load_plugins(plugin_name):
+    punda = "KawaiiXRobot/plugins/*.py"
+    devu = glob.glob(path)
     path = Path(f"KawaiiXRobot/plugins/{plugin_name}.py")
     name = "KawaiiXRobot.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
@@ -14,7 +16,7 @@ def load_plugins(plugin_name, files):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["KawaiiXRobot.plugins." + plugin_name] = load
-    print("Total Plugins -->" + len(files))
+    print("Total Plugins -->" + len(devu))
     print("Imported --> " + plugin_name)
 
 path = "KawaiiXRobot/plugins/*.py"
