7aa6bfef57cf4e89c5fc89cc0524f004103efd31
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-07 17:32:20 +0530

    Update scanlist.py

--- KawaiiXRobot/plugins/scanlist.py
+++ KawaiiXRobot/plugins/scanlist.py
@@ -8,16 +8,15 @@ from KawaiiXRobot.utils.dbfunctions import gbansdb
 async def list(client, message):
    total_num = len([i async for i in gbansdb.find({"user_id": {"$gt": 0}})])
    total_id = ([i['user_id'] async for i in gbansdb.find({"user_id": {"$gt": 0}})])
-   n = 0
    reply = f"`{total_num}` **Globally Scanned/Banned Users\n**"
+   global total_id
    for x in total_id:
        try: 
            user = await bot.get_users(int(x))
            mention = "[" + user.first_name + "](tg://user?id=" + str(user.id) + ")"
-           n += 1
        except Exception:
            continue
-           reply += f"{n}. {mention}\n"
+           reply += f"• {mention}\n"
        if reply == "":
            return await message.reply_texf(text="`No Scanned/Banned Found.`")
        else:
