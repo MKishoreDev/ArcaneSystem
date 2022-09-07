3197237d6118cac1f6d774487f057ca6b471e0f7
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-07 21:55:41 +0530

    `gs added `

--- KawaiiXRobot/plugins/telecaller.py
+++
@@ -1,9 +0,0 @@
-from pyrogram.types import InputPhoneContact
-from pyrogram import filters
-from KawaiiXRobot import ubot
-
-@ubot.on_message(filters.command("find"))
-async def phonenumber(_, message):
-    text = message.text.split(None, 1)[1]
-    number = await ubot.import_contacts([InputPhoneContact("+91" + text, "Foo")])
-    await message.reply_text(number)
