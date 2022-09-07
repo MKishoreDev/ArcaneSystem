2a6b0d36f8442747b3cb854ccbca9abb65c6b178
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-07 19:24:31 +0530

    😑😑

--- KawaiiXRobot/plugins/telecaller.py
+++ KawaiiXRobot/plugins/telecaller.py
@@ -4,9 +4,6 @@ from KawaiiXRobot import ubot
 
 @ubot.on_message(filters.command("find"))
 async def find(_, message):
-              if len(message.command) <8:
-                  await message.reply("Put Correct Number")
-                  return 
               elif message.reply_to_message:
                   Numb = message.text.replace("/find", "")
                   await ubot.send_message([InputPhoneContact(f"{Numb}", "Foo")])
