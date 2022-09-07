b2433185003bc8564ba81b68aee9167566594604
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-07 20:00:36 +0530

    Update telecaller.py

--- KawaiiXRobot/plugins/telecaller.py
+++ KawaiiXRobot/plugins/telecaller.py
@@ -5,6 +5,5 @@ from KawaiiXRobot import ubot
 @ubot.on_message(filters.command("find"))
 async def phonenumber(_, message):
     text = message.text.split(None, 1)[1]
-    India = "+91"
-    number = await ubot.import_contacts([InputPhoneContact(India, text, "Foo")])
+    number = await ubot.import_contacts([InputPhoneContact(+91, text, "Foo")])
     await message.reply_text(number)
