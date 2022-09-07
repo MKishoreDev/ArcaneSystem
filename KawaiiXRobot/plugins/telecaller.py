a09d95670c630396c93aabf5bc3d8e1c8016e9a9
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-07 19:05:38 +0530

    Update telecaller.py

--- KawaiiXRobot/plugins/telecaller.py
+++ KawaiiXRobot/plugins/telecaller.py
@@ -3,6 +3,6 @@ from KawaiiXRobot import ubot
 
 @ubot.on_message(filters.command("find"))
 async def find(client, message):
-cmd = message.text.split(" ", maxsplit=1)[1]
-      await ubot.send_message([InputPhoneContact(f"{cmd}, "Foo")])
+     cmd = message.text.split(" ", maxsplit=1)[1]
+     await ubot.send_message([InputPhoneContact(f"{cmd}, "Foo")])
       
