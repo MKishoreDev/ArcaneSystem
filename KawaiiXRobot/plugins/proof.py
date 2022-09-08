a685bb04a82d5117bfce7ccb1823c1db137b8e29
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-08 10:18:07 +0530

    ` Proof © By @HMF_Owner_1 😂 `

---
+++ KawaiiXRobot/plugins/proof.py
@@ -0,0 +1,31 @@
+import asyncio
+from KawaiiXRobot import bot, db
+from pyrogram import filters
+from config import DEVS, Inspector, Enforcer
+
+HMF = DEVS + Inspector + Enforcer 
+
+@bot.on_message(filters.command("addproof"))
+def proofss(_, m: Message):
+    proof = m.text.replace(m.text.split(" ")[0], "")
+    if not proof == "":
+        db.add_proof(m.from_user.id, proof)
+        m.reply("Dᴏɴᴇ!")
+    else:
+        m.reply("Usᴀɢᴇ : /addproof 12345678 telegraph link")
+
+@bot.on_message(filters.command("getproof", ['/', ".", "?"]))
+    if db.get_proof(m.from_user.id)['proofs'] != True:
+        status = "Proof Not Stored"
+
+    elif db.get_proof(m.from_user.id)['proofs'] == True:
+        status = db.get_role(m.from_user.id)['proof']
+
+    else:
+        status = "Proof Not Stored"
+
+    if m.from_user.id in HMF:
+        dev_text = f"""
+• Proof: {proofs}
+"""
+    await m.reply(text)
