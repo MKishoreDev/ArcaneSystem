4cba4242aec74efdfd2085d0ac1032388f7c46e4
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-08 15:02:07 +0530

    Update proof.py

--- KawaiiXRobot/plugins/proof.py
+++ KawaiiXRobot/plugins/proof.py
@@ -1,18 +1,26 @@
 from pyrogram import filters, Client
 from KawaiiXRobot import bot, db, DEVS
 
+def parse_com(com, key):
+  try:
+    r = com.split(key,1)[1]
+  except KeyError:
+    return None
+  r = (r.split(" ", 1)[1] if len(r.split()) >= 1 else None)
+  return r
+
 @bot.on_message(filters.command(["proof"], ['/', ".", "?"]) & filters.user(DEVS))
 async def proof(client, message):
-    data_id = message.text.split(None, 1)[1]
+    data_id = parse_com(message.text, "proof")
     if len(message.command) <1:
        await message.reply_text("`Usage:` `/proof 1 fuck`")
     elif len(message.command) <2:
        await message.reply_text("`Give Proof For The Given Id")
+    if not message.reply_to_message:
+       store = parse_com(message.text, f"proof {data_id}")
     if message.reply_to_message:
        store = message.reply_to_message.text
-    if not message.reply_to_message:
-       store = message.text.split(None, 2)[2]
-    if message.reply_to_message.sticker:
+    elif message.reply_to_message.sticker:
        store = message.reply_to_message.sticker.file_id
     elif message.reply_to_message.photo:
        store = message.reply_to_message.photo.file_id
@@ -32,7 +40,7 @@ async def proof(client, message):
 
 @bot.on_message(filters.command(["data"], ['/', ".", "?"]))
 async def data(client, message):
-    data_id = message.text.split(None, 1)[1]
+    data_id = parse_com(message.text, "data")
     if len(message.command) <1:
        await message.reply_text("`Usage:` `/data 1`")
     if db.get_proof(data_id)['status'] == True:
