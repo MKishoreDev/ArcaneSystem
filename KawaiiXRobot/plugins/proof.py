fc5481f088a2ff640aeeab32311e2a2e406b97c5
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-08 14:20:36 +0530

    Update proof.py

--- KawaiiXRobot/plugins/proof.py
+++ KawaiiXRobot/plugins/proof.py
@@ -3,15 +3,16 @@ from KawaiiXRobot import bot, db, DEVS
 
 @bot.on_message(filters.command(["proof"], ['/', ".", "?"]) & filters.user(DEVS))
 async def proof(client, message):
-    data_id = message.text.split(None, 1)[1]
     if len(message.command) <1:
        await message.reply_text("`Usage:` `/proof 1 fuck`")
     elif len(message.command) <2:
        await message.reply_text("`Give Proof For The Given Id")
     if message.reply_to_message:
        store = message.reply_to_message.text
+       data_id = message.text.split(None, 1)[1]
     if not message.reply_to_message:
        store = message.text.split("", 2)[2]
+       data_id = message.text.split(None, 1)[1]
     if db.get_proof(data_id)['status'] == True:
        await message.reply_text("`The Proof For This Id Was Already Added Remove The Proof And Try Again`")
     if db.get_proof(data_id)['status'] != False:
