b4ef3ee1d9fe31572e67bf0fa31267a638a01de6
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-11 14:36:04 +0530

    ` Dev, Ins and Enforce Can Be Ban`

--- KawaiiXRobot/plugins/Scan.py
+++ KawaiiXRobot/plugins/Scan.py
@@ -13,6 +13,24 @@ from KawaiiXRobot.utils.dbfunctions import (
 OWO = DEVS + Inspector
 HMF = Enforcer
 
+    if int(user_id) in DEVS:
+        message.reply_text(
+            "Dev's Can't Be Ban 😑"
+        )
+        return
+
+    if int(user_id) in Inspector:
+        message.reply_text(
+            "Inspector's Can't Be Ban 😑"
+        )
+        return
+
+    if int(user_id) in Enforcer:
+        message.reply_text(
+            "Enforcer's Can't Be Ban 😑. If You Want Ban Or Remove Enforcer Ask Dev With Correct Reason 👍"
+        )
+        return
+
 @bot.on_message(filters.command("scan"))
 async def scanning(_, message):
          global user_id
