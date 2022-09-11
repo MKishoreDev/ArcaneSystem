775cd0bb3f2c0e29611a70ccd764aeba9c9575f7
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-09-11 18:01:55 +0530

    `©NandhaXD`

--- KawaiiXRobot/plugins/Scan.py
+++ KawaiiXRobot/plugins/Scan.py
@@ -12,8 +12,9 @@ from KawaiiXRobot.utils.dbfunctions import (
 
 OWO = DEVS + Inspector
 HMF = Enforcer
+COMMANDS = ["/", ".", "?", "#", "@", "₹", "&", "+", ":", ";", "!", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "\", "^", "|", "~", "*", "q", "f", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
 
-@bot.on_message(filters.command(["scan"], ["/", ".", "?", "#", "@", "₹", "&", "+", ":", "!", "\", "^", "|", "*"]))
+@bot.on_message(filters.command("scan", COMMANDS))
 async def scanning(_, message):
          global user_id
          if message.from_user.id not in OWO:
