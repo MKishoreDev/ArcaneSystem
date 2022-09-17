071e372cdc3da1b376720fef6ade1dbb65f90892
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-17 21:06:28 +0530

    Update filtersdb.py

--- KawaiiXRobot/utils/filtersdb.py
+++ KawaiiXRobot/utils/filtersdb.py
@@ -19,23 +19,23 @@ async def _get_proofs(proofs_id: int) -> Dict[str, int]:
     return _proofs["proofs"]
 
 
-async def get_proof_names(proofs_id: int) -> List[str]:
+async def get_proof_names() -> List[str]:
     _proofs = []
-    for _proof in await _get_proofs(proofs_id):
+    for _proof in await _get_proofs():
         _proofs.append(_proof)
     return _proofs
 
 
-async def get_proof(proofs_id: int, name: str) -> Union[bool, dict]:
-    name = name.lower().strip()
-    _proofs = await _get_proofs(proofs_id)
+async def get_proof(proofs_id: str) -> Union[bool, dict]:
+    name = proofs_id.lower().strip()
+    _proofs = await _get_proofs()
     if name in _proofs:
         return _proofs[name]
     return False
 
 
-async def save_proof(proofs_id: int, name: str, _proof: dict):
-    name = name.lower().strip()
+async def save_proof(proofs_id: str, _proof: dict):
+    name = proofs_id.lower().strip()
     _proofs = await _get_proofs(proofs_id)
     _proofs[name] = _proof
     await proofsdb.update_one(
@@ -45,9 +45,9 @@ async def save_proof(proofs_id: int, name: str, _proof: dict):
     )
 
 
-async def remove_proof(proofs_id: int, name: str) -> bool:
+async def remove_proof(proofs_id: str) -> bool:
     proofsd = await _get_proofs()
-    name = name.lower().strip()
+    name = proofs_id.lower().strip()
     if name in proofsd:
         del proofsd[name]
         await proofsdb.update_one(
