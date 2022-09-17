a202223e2fdac21aaa6661bf9a6b7b0a847a6e97
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-09-17 13:09:18 +0530

    ` Bot Status Added | © Copyright @DeepakJackson `

---
+++ KawaiiXRobot/plugins/Bot_Status.py
@@ -0,0 +1,47 @@
+# © Copyright By @DeepakJackson
+
+import os
+import pytz
+import asyncio
+import datetime
+from KawaiiXRobot import ubot
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+
+async def statusbots():
+    async with ubot:
+            while True:
+                print("Checking...")
+                GET_CHANNEL_OR_GROUP = await ubot.get_chat(int(CHANNEL_OR_GROUP_ID))
+                CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
+                CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
+                checker_bot = f"💡 **<u>LIVE BOT STATUS</u>** 💡\n\n💬 **{CHANNEL_OR_GROUP_NAME}**"
+                for bot in BOT_LIST:
+                    try:
+                        checker_status = await ubot.send_message(bot, "/start")
+                        aaa = checker_status.message_id
+                        await asyncio.sleep(10)
+                        checker_user = await ubot.get_history(bot, limit = 1)
+                        for ccc in checker_user:
+                            bbb = ccc.message_id
+                        if aaa == bbb:
+                            checker_bot += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: down ❌"
+                            for bot_admin_id in BOT_ADMIN_IDS:
+                                try:
+                                    await ubot.send_message(int(bot_admin_id), f"🚨 **announcement** 🚨\n\n» @{bot} is down** ❌")
+                                except Exception:
+                                    pass
+                            await ubot.read_history(bot)
+                        else:
+                            checker_bot += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: alive ✅"
+                            await ubot.read_history(bot)
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)            
+                time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+                last_update = time.strftime(f"%d %b %Y at %I:%M %p")
+                checker_bot += f"\n\n🛂 Last Check: {last_update} ({TIME_ZONE})\n\n🟡 **updates every 45 min(s)**\n\n⚡ Powered by:- @shukurenai007, @Pegasus_Network"
+                await ubot.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, checker_bot)
+                print(f"Last Check At: {last_update}")                
+                await asyncio.sleep(2700)
+                        
+ubot.run(statusbots())
