e9b19a560dc0d97a978a180ba76444b3a8d6e347
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-09-24 08:07:43 +0530

    Create add_ins.py

---
+++ KawaiiXRobot/utils/add_ins.py
@@ -0,0 +1,31 @@
+import codecs
+import pickle
+from typing import Dict, List, Union
+
+from KawaiiXRobot import db
+
+Inspectordb = db.sudoers
+
+async def get_Inspector() -> list:
+    sudoers = await sudoersdb.find_one({"Inspector": "Inspector"})
+    if not Inspector:
+        return []
+    return Inspector["Inspector"]
+
+
+async def add_Inspector(user_id: int) -> bool:
+    Inspector = await get_Inspector()
+    Inspector.append(user_id)
+    await Inspectordb.update_one(
+        {"Inspector": "Inspector"}, {"$set": {"Inspector": Inspector}}, upsert=True
+    )
+    return True
+
+
+async def remove_Inspector(user_id: int) -> bool:
+    Inspector = await get_sudoers()
+    Inspector.remove(user_id)
+    await Inspectordb.update_one(
+        {"Inspector": "Inspector"}, {"$set": {"Inspector": Inspector}}, upsert=True
+    )
+    return True
