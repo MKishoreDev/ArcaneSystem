61377dfa81d9b5b89dd7f6f437a0f20d46c28581
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-09-30 12:09:24 +0530

    Update unscan.py

--- KawaiiXRobot/plugins/unscan.py
+++ KawaiiXRobot/plugins/unscan.py
@@ -17,20 +17,48 @@ COMMANDS = ["/", ".", "?", "#", "@", "₹", "+", ":", "!", "^", "|"]
 OWO = DEVS + Inspector
 
 @bot.on_message(filters.command("revert", COMMANDS))
-async def revive(_, message):
-          
-          if message.from_user.id in OWO and message.reply_to_message:
-                       await x.remove_gban_user(message.reply_to_message.from_user.id)
-                       await message.reply(f"Kawai User: {message.from_user.id}\n target user: {message.reply_to_message.from_user.id}\n **UNBANNED**")  
-          
-          else:
-                if message.from_user.id not in OWO:
-                      return await message("Only OWO user can use")
-                elif len(message.command) <2:
-                      return await message.reply("give a user ID or name")
-                elif not message.reply_to_message:
-                     user_id_text = message.text.split(None, 1)[1]
-                     user = await bot.get_users(user_id_text)
-                     await x.remove_gban_user(user.id)
-                     await message.reply(f"Kawai User: {message.from_user.id}\n target user: {user.id}\n **UNBANNED**")
-             
+async def revert(_, message):
+         global user_id
+         if message.from_user.id not in OWO:
+              return await message.reply_text("Sorry bitch your not my own user") 
+         if len(message.command) <2:
+                  await message.reply("*Any reason to Unscan ?")
+                  return 
+         elif message.reply_to_message:
+                user_id = message.reply_to_message.from_user.id
+                admire = message.from_user.id
+                reason = message.text.replace("/revert", "")
+
+         if int(user_id) in DEVS:
+             await message.reply_text(
+                 "Dev's Can't Be UnBan😑"
+             )
+             return
+
+         if int(user_id) in Inspector:
+             await message.reply_text(
+                 "Inspector's Can't Be UnBan 😑"
+             )
+             return
+
+         if int(user_id) in Enforcer:
+             await message.reply_text(
+                 "Enforcer's Can't Be UnBan 😑. If You Want Ban Or Remove Enforcer Ask Dev With Correct Reason 👍"
+             )
+             return
+                       
+         msg = await message.reply_text("**Connecting to Cringe System...**")
+         await x.remove_gban_user(user_id)
+         text = f"""#UnScanned
+
+**From Chat:** {message.chat.title}
+**Admire:** [{admire}](tg://user?id={admire})
+**UnScanned:** [{user_id}](tg://user?id={user_id})
+**Reason**: {reason}
+**Event Stamp:** {Current_Date_Time}
+"""
+
+         await ubot.send_message(-1001781501832, f"/ugban {user_id} {reason}")
+         await bot.send_message(-1001723857695, text)
+         await msg.edit_text(f"Successfully Scanned [{user_id}](tg://user?id={user_id})")
+
