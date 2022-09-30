1c668792525866ed99ba71c45ee6155cec057afb
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-30 20:39:58 +0530

    Create god_aasf.py

---
+++ KawaiiXRobot/plugins/god_aasf.py
@@ -0,0 +1,52 @@
+from KawaiiXRobot import DEVS, bot
+from KawaiiXRobot.utils.adddb import add_enf, rm_enf, enf_list
+from pyrogram import filters
+from pyrogram.errors import RPCError
+
+@bot.on_message(filters.command("addenf"))
+async def add_enf(_, message):
+	chat_id = message.chat.id
+	if message.reply_to_message:
+		user_id = message.reply_to_message.from_user.id
+		mention = message.reply_to_message.from_user.mention
+	else:
+		args = message.text.split(None,2)
+		try:
+			user = await bot.get_users(args[1])
+			user_id = user.id
+			mention = user.mention
+		except RPCError:
+			await message.reply_text("`User Not Found`")
+			return
+	add_enf(user_id)
+	await message.reply_text("Successfully Added To Enforcer")
+
+@bot.on_message(filters.command("rmenf"))
+async def rm_enf(_, message):
+	chat_id = message.chat.id
+	if message.reply_to_message:
+		user_id = message.reply_to_message.from_user.id
+		mention = message.reply_to_message.from_user.mention
+	else:
+		args = message.text.split(None,2)
+		try:
+			user = await bot.get_users(args[1])
+			user_id = user.id
+			mention = user.mention
+		except RPCError:
+			await message.reply_text("`User Not Found`")
+			return
+
+	rm_enf(user_id)
+	await message.reply_text("Successfully Removed User From Enf")
+
+@bot.on_message(filters.command("enflist"))
+async def enflist(_, message):
+	chat_id = message.chat.id
+        text = "Enforcers List:"
+	enf_list = enf_list()
+	for enf in enf_list:
+		user = await bot.get_users(enf.user_id)
+		mention = user.mention
+		text += "\n - {}".format(mention)
+	await message.reply_text(text)
