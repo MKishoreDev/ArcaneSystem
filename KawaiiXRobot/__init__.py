c97e20017986acb46487a7016d4cb154cb2762b7
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-09-30 07:09:38 +0530

    Update __init__.py

--- KawaiiXRobot/__init__.py
+++ KawaiiXRobot/__init__.py
@@ -16,8 +16,8 @@ KAWAII_LOGS = os.environ.get("KAWAII_LOGS", None)
 KAWAII_CHANNEL = os.environ.get("KAWAII_CHANNEL", None)
 DEVS = os.environ.get("DEVS", None)
 Inspector = os.environ.get("Inspector", None)
-Enforcer = os.environ.get("Enforcer", None)
-PYRO_SESSION = "BQDqqZsAGsT3prxh157M2aPeX6Za0vgOOlDVPFyv-_Sf9nTXAhj1JR7567Ts59ZV3f7e-CLCwKqbIJFYzoprzXinPprx93_gJNK3OgCIaBD2tj-KIa1M_zHa6tB3PwoaInDVUfLDfMd3igOkBk-l08DsvG1WbiCYyIsQxEM8clyZiJU0vVXBhfKJ-Lsg7_7YJJD5mPAZwIVbUcmr3Mlan-YW42vp1Aet8D4WaS1W9EOq57r7NRY-mUGZwrdiRaz7coETfyCssIhriZWkHdzntAwZI8uQAIDuuHET4Y59OyuH7ed82HoJ5u_n4AUBnCuqDi1yeWRy0LJGtL_cHFGeLtKD_tio-gAAAAFCPvD-AA"                            
+Enforcer = os.environ.get("Enforcer", None)                         
+PYRO_SESSION = environ.get("PYRO_SESSION", None)
 DEVS = [
  5175767264, 1989750989, 1544286112, 1491497760, 5446914371
 ]
