f3a0c652ece47f4b7e2611ce7c327d6cb0980c2b
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-03 21:13:03 +0530

    Update Add2.py

--- KawaiiXRobot/plugins/Add2.py
+++ KawaiiXRobot/plugins/Add2.py
@@ -1,39 +1,56 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from KawaiiXRobot import Inspector, bot, DEVS
-from KawaiiXRobot.utils.errors import capture_err
-from KawaiiXRobot.utils.add_ins import add_Inspector, get_Inspector, remove_Inspector
+from KawaiiXRobot import ENFORCERS, bot, DEVS
 
-COMMANDS = ["/", ".", "?", "#", "@", "₹", "+", ":", "!", "^", "|"]
+@bot.on_message(filters.command("addenf", COMMANDS) & filters.user(DEVS))
+def addenf(_, m: Message):
+    if m.reply_to_message:
+        user_id = m.reply_to_message.from_user.id
+    if len(message.command) <2:
+        user_id = m.text.replace(m.text.split(" ")[0], "")
+    if not user_id:
+        m.reply_text("`Refer A User First...`")
+        return 
+    if int(user_id) == DEVS:
+        message.reply_text("The specified user is my dev! No need add him to Enforcer list!")
+        return ""
 
-BOT_ID = 5620916588
+    if int(user_id) in ENFORCERS:
+        message.reply_text("Buddy this user is already a enforcer user.")
+        return ""
 
-@bot.on_message(filters.command("useradd", COMMANDS) & DEVS)
-@capture_err
-async def useradd(_, message: Message):
-    if not message.reply_to_message:
-        return await message.reply_text(
-            message,
-            text="Reply to someone's message to add him to sudoers.",
-        )
-    user_id = message.reply_to_message.from_user.id
-    umention = (await bot.get_users(user_id)).mention
-    Inspector = await get_Inspector()
+    with open("enf_users.txt","a") as file:
+        file.write(str(user_id) + "\n")
 
-    if user_id in Inspector:
-        return await message.reply_text(message, text=f"{umention} is already in sudoers.")
-    if user_id == BOT_ID:
-        return await message.reply_text(
-            message, text="You can't add assistant bot in sudoers."
-        )
+    ENFORCERS.append(user_id)
+    m.reply_text("Succefully Added To SUDO List!")
+    return
 
-    await add_Inspector(user_id)
+@bot.on_message(filters.command("rmenf", COMMANDS) & filters.user(DEVS)
+def rmenf(_, m: Message):
+    if m.reply_to_message:
+        user_id = m.reply_to_message.from_user.id
+    if len(message.command) <2:
+        user_id = m.text.replace(m.text.split(" ")[0], "")
+    if not user_id:
+        m.reply_text("`Refer A User First...`")
+        return 
+    if int(user_id) == DEVS:
+        message.reply_text("The specified user is my dev no need to remove from anything Blah Blah!")
+        return ""
 
-    if user_id not in Inspector:
-        Inspector.add(user_id)
+    if user_id not in ENFORCERS:
+        message.reply_text("Buddy this user is not a enforcer.")
+        return ""
 
-    await message.reply_text(
-        message,
-        text=f"Successfully added {umention} in sudoers.",
-    )
+    users = [line.rstrip('\n') for line in open("enf_users.txt")]
+
+    with open("enf_users.txt","w") as file:
+        for user in users:
+            if not int(user) == user_id:
+                file.write(str(user) + "\n")
+
+    ENFORCERS.remove(user_id)
+    message.reply_text("Yep Succefully removed from Enf List!")
+    return
