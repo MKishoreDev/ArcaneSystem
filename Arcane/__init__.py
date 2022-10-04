6688b5d87ac111c521a39ef93170c167173b9172
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-04 20:22:38 +0530

    Update __init__.py

--- Arcane/__init__.py
+++ Arcane/__init__.py
@@ -4,8 +4,8 @@ from pyrogram import Client
 from aiohttp import ClientSession
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-
 API_ID = os.environ.get("API_ID", None)
+APP_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 TOKEN = os.environ.get("TOKEN", None)
 MONGO_URI = os.environ.get("MONGO_URI", None)
@@ -17,7 +17,7 @@ PREFIX = ["/", ".", "?", "#", "@", "₹", "+", ":", "!", "^", "|"]
 MONGO_URL = "mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority"
 SESSION = "BQDqqZsApNgeje1PGDqTNrmkU9DmvN9Jj1Mo_ColTGGbHosp7dOBSpdOMU--2MdJ29SDE1dzGtqptFXjwe92dtw00sUMf3zkypDuyg0kfUk_toh_vdYXqu3XOy78yA9mOb7c9mbiLckjc9fpRXLJdyjV4SqRpem9CuCWVT9AzalnRYHpGqIP99qg4MTOfp5Y1Cakc7RM_CCMShzzY_WRm1IkWiE2zTUBWU_Fn9Lz-pyHy1sHFrcuT4Utf2qJAu5EicRbXT8oqtrqpJ14UAuc4LaULbT-PXPs95iGZfC5PjdhFQMz9EkTA-3Vo8bMEBToFe5smVdzSAyqcyhlNEMpE_ukjoWb9QAAAAFCPvD-AA"
 
-bot = Client("Arcane", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+bot = Client("Arcane", api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
 ubot = Client(session_string=SESSION, api_id=API_ID, api_hash=API_HASH, name="ArcaneSystem") 
 mongo_client = MongoClient(MONGO_URL)
 StartTime = time.time()
