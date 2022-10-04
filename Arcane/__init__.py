2add3f1790f005c8ba9fb5907d0258c038225ab6
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-04 20:27:34 +0530

    Update __init__.py

--- Arcane/__init__.py
+++ Arcane/__init__.py
@@ -4,12 +4,15 @@ from pyrogram import Client
 from aiohttp import ClientSession
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
+# API_HASH & API_ID FOR UBOT
 API_ID = os.environ.get("API_ID", None)
-APP_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
+SESSION = os.environ.get("SESSION", None)
+# APP_ID & APP_HASH FOR BOT
+APP_ID = os.environ.get("APP_ID", None)
+APP_HASH = os.environ.get("APP_HASH", None)
 TOKEN = os.environ.get("TOKEN", None)
 MONGO_URI = os.environ.get("MONGO_URI", None)
-SESSION = os.environ.get("SESSION", None)
 DEVS = [
  5696053228, 5446914371, 1544286112, 1989750989
 ]
