850cb25e53af0e66e2bdebff52e8b9cb5cb0082d
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-04 21:23:24 +0530

    Update media.py

--- Arcane/media.py
+++ Arcane/media.py
@@ -1,4 +1,4 @@
-ANIMATION_MEDIA = "https://telegra.ph/file/c72f6d8d50365242bf7a1.jpg"
+ANIMATION_MEDIA = "https://telegra.ph/file/7100024f523ebb13dc75a.mp4"
 START_MEDIA = "https://telegra.ph/file/7100024f523ebb13dc75a.mp4"
 HELP_MEDIA = "https://telegra.ph/file/80f59b1adfd42a368552a.jpg"
 RULES_MEDIA = "https://telegra.ph/file/ddda05f25ecfb52f1029a.mp4"
