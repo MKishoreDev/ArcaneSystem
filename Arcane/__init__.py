37199762ea24b8eb8828d12b1a1ded451ba1cc5d
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-04 15:27:27 +0530

    Update __init__.py

--- Arcane/__init__.py
+++ Arcane/__init__.py
@@ -1,50 +1,19 @@
-import os
 import time
+
 from pyrogram import Client
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from Arcane.utils.db import DATABASE
 
-from telethon import TelegramClient
-from telethon.sessions import MemorySession
-
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-MONGO_URI = os.environ.get("MONGO_URI", None)
-KAWAII_LOGS = os.environ.get("KAWAII_LOGS", None)
-KAWAII_CHANNEL = os.environ.get("KAWAII_CHANNEL", None)
-DEVS = os.environ.get("DEVS", None)
-Inspector = os.environ.get("Inspector", None)
-Enforcer = os.environ.get("Enforcer", None)                         
-PYRO_SESSION = os.environ.get("PYRO_SESSION", None)
+API_ID = ""
+API_HASH = ""
+TOKEN = ""
 DEVS = [
- 5175767264, 1989750989, 1544286112, 1491497760, 5446914371
-]
-Inspector = [
- 1057561033, 5446914371
-]
-Enforcer = [
- 1891633746, 5446914371, 1994968540
-]
-BOT_ID = [
- 5620916588
+ 5696053228, 5446914371, 1544286112, 1989750989
 ]
 
-ENFORCERS = []
+MONGO_URL = "mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority"
+SESSION = ""
 
-if 5446914371 not in DEVS:
-   DEVS.append(5446914371)
-
-bot = Client("cringe_X_Robot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-db = DATABASE(MONGO_URI)
-mongo_client = MongoClient("mongodb+srv://Music:Music@cluster0.f9x4i.mongodb.net/Cluster0?retryWrites=true&w=majority")
-mdb = mongo_client.gof
-proofsdb = mongo_client.proofsy
-ubot = Client(session_string=PYRO_SESSION, api_id=API_ID, api_hash=API_HASH, name="CringeUser") #nandhaxd
-tbot = TelegramClient(MemorySession(), API_ID, API_HASH)
-
+bot = Client("Arcane", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+ubot = Client(session_string=SESSION, api_id=API_ID, api_hash=API_HASH, name="ArcaneSystem") 
+mongo_client = MongoClient(MONGO_URL)
 StartTime = time.time()
-ubot.start()
-tbot.start(bot_token=BOT_TOKEN)
-
-print("Starting The Cringe X System")
