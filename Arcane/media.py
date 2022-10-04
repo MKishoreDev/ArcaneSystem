c0db71f7f1dddad1bdaf17234d3650218d470a55
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-04 16:50:17 +0530

    Update media.py

--- Arcane/media.py
+++ Arcane/media.py
@@ -1,2 +1,3 @@
+ANIMATION_MEDIA = "https://telegra.ph/file/7d6f9c51b2cbdea29ee84.jpg"
 START_MEDIA = "https://telegra.ph/file/7100024f523ebb13dc75a.mp4"
 RULES_MEDIA = "https://telegra.ph/file/80f59b1adfd42a368552a.jpg"
