e14d978df4a306ba30d44154d748bc7c1c3baf9e
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-04 19:10:21 +0530

    Update strings.py

--- Arcane/strings.py
+++ Arcane/strings.py
@@ -5,3 +5,29 @@ START_STRING = """
 ━━━━━━━━━━━━━━━━━
 **Click The Below Help Button To Know About All My Commands Or Tap On Rules Button To Read Rules**
 """
+
+INSPECTOR_RULES = """**Inspector Rules**
+• `don't scan without reason to anyone`
+• `don't scan Without Proof, scan With Correct Proof`
+• `don't scan your personal issues`
+• `don't scan first check the reason carefully` 
+• `don't scan private chat because were don't trust private things`
+• `don't give proof with clear pornography proof Hide and add reason`"""
+
+G_B_RULES = """**Girls And Boys Safe Zone**
+• `Don't Abuse Anyone, If You Abuse AnyOne Then You Will Get Ban From AnyOne`
+• `Porn Girls And Gay Boys Are Not Allowed In Group`"""
+
+GROUP_RULES = """**Group Rules**
+• `Do not use profanity`
+• `Do not advertise without admins permission.`
+• `Do not flood the group (No spamming).`
+• `Gore, Porn, Hentai or any sort of NSFW is strictly prohibited. Do not put up explicit content.`
+• `Do not "disturb" anyone in personal messages.`
+• `Do Not use bad Words On VC.`
+• `Don't Fight On A personal Reasons. also on a vc you can't.`"""
+
+TOXIC_RULES = """**Toxic Rules** 
+• `Use Less Toxic For Your Entertainment`
+• `Don't Use Unwanted`""
+
