ce3fcb86c4119c0d9902a902c0d3716d4e4a0121
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-06 05:21:29 +0530

    Update __init__.py

--- Arcane/__init__.py
+++ Arcane/__init__.py
@@ -1,7 +1,7 @@
 import os
 import time
 from pyrogram import Client
-from Arcane.utils.enfsdb import get_enfusers
+from Arcane.utils.addenf_db import get_enfusers
 from pymongo import MongoClient
 from aiohttp import ClientSession
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
