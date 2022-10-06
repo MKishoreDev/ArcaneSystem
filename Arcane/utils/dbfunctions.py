dd7fb4f2b5fd26a5ed2c904d62801e978b450fc4
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-06 08:30:20 +0530

    Update dbfunctions.py

--- Arcane/utils/dbfunctions.py
+++ Arcane/utils/dbfunctions.py
@@ -1,6 +1,7 @@
 from Arcane import mdb
 
 gbansdb = mdb.gban
+ranksdb = mdb.rankuserdb
 
 async def is_gbanned_user(user_id: int) -> bool:
     user = await gbansdb.find_one({"user_id": user_id})
@@ -32,3 +33,27 @@ async def get_gbans_id() -> int:
 
 async def get_gbans_data() -> int:
     return ([i['user_id'] async for i in gbansdb.find({"user_id": {"$gt": 0}})])
+
+
+
+
+
+async def get_rankusers() -> list:
+    rank_users = await ranksdb.find_one({"user_id": "user_id"})
+    if not rank_users:
+        return []
+    return rank_users["rank_users"]
+
+async def add_rank(user_id: int):
+      rank_users = await get_rankusers()
+      rank_users.append(user_id)
+      await ranksdb.update_one(
+        {"user_id": "user_id"}, {"$set": {"rank_users": rank_users}}, upsert=True)
+      return True  
+
+async def remove_rank(user_id: int):
+       rank_users = await get_rankusers()
+       rank_users.remove(user_id)
+       await ranksdb.update_one(
+        {"user_id": "user_id"}, {"$set": {"rank_users": rank_users}}, upsert=True)
+       return True
