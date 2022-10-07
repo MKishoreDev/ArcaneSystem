6b3ae31c6f1b975b7c66a9623bbc97270ccec508
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-07 20:03:22 +0530

    Update ranks.py

--- Arcane/ranks.py
+++ Arcane/ranks.py
@@ -1,5 +1,11 @@
 from Arcane.utils.dbfunctions import get_enforcers
+from Arcane.utils.dbfunctions import get_Inspector
 
 async def Enforcers():
      list = (await get_enforcers())
      return list
+
+async def Inspector():
+     list = (await get_Inspector())
+     return list
+
