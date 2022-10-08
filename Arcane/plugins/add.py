1d6ea457443bc844af39d7baf6b01ea35900b338
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-08 07:39:24 +0530

    Update add.py

--- Arcane/plugins/add.py
+++ Arcane/plugins/add.py
@@ -1,5 +1,5 @@
 from pyrogram import filters
-from Arcane import bot, DEVS
+from Arcane import bot, DEVS, PREFIX
 from Arcane.utils.dbfunctions import (
 add_enforcers, remove_enforcers, get_enforcers)
 from Arcane.ranks import Enforcers 
@@ -8,9 +8,24 @@ from Arcane.utils.dbfunctions import (
 add_Inspector, remove_Inspector, get_Inspector)
 from Arcane.ranks import Inspectors_list
 
+@bot.on_message(filters.command("add", PREFIX))
+async def start(_, m: Message):
+         global user_id
+         if message.from_user.id not in DEVS:
+              return await message.reply_text("WTF! You'er My Developer") 
+         if message.reply_to_message:
+                await m.reply_video(ADD_MEDIA,
+                caption=ADD_STRIMG.format(
+                m.from_user.mention
+               ),
+              reply_markup=InlineKeyboardMarkup(ADD_BUTTON),
+           )
+
 @bot.on_callback_query(filters.regex("addinss"))
 async def addinss(_, query):
       if query.from_user.id in DEVS
+      user_id = message.reply_to_message.from_user.id
+      admin = message.from_user.id
       await add_Inspector(user_id)
-      text = f""" Congratulations [{query.from_user.id}](tg://user?id={query.from_user.id}) Into Inspector"""
+      text = f"""  [{admin}](tg://user?id={admin}) Promoted [{query.from_user.id}](tg://user?id={query.from_user.id}) Into Inspector"""
       await query.message.edit(text, reply_markup=InlineKeyboardMarkup(PROINS_BUTTON))
