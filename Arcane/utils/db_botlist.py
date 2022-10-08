2886cc35bef061069e4c1285646fe3c9b17667f2
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-08 23:40:46 +0530

    Create db_botlist.py

---
+++ Arcane/utils/db_botlist.py
@@ -0,0 +1,35 @@
+from Arcane import mdb 
+
+botsdb = mdb.bots
+
+async def is_Boted_user(user_id: int) -> bool:
+    user = await botsdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    else:
+        return True
+
+
+async def add_bots_user(user_id: int):
+    is_Boted = await is_Boted_user(user_id)
+    if is_Boted:
+        return
+    return await botsdb.insert_one({"user_id": user_id})
+
+
+async def remove_bots_user(user_id: int):
+    is_Boted = await is_Boted_user(user_id)
+    if not is_Boted:
+        return
+    return await botsdb.delete_one({"user_id": user_id})
+
+
+async def get_bots_count() -> int:
+    return len([i async for i in botsdb.find({"user_id": {"$gt": 0}})])
+
+async def get_bots_id() -> int:
+    return str([i async for i in botsdb.find({"user_id": {"$gt": 0}})])
+
+async def get_bots_data() -> int:
+    return ([i['user_id'] async for i in botsdb.find({"user_id": {"$gt": 0}})])
+
