c6b694f45f0f376eee1e29e254889ba4723ea456
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-08 08:27:37 +0530

    Update add.py

--- Arcane/plugins/add.py
+++ Arcane/plugins/add.py
@@ -5,11 +5,11 @@ from Arcane.buttons import *
 from Arcane import bot, DEVS, PREFIX
 from Arcane.utils.dbfunctions import (
 add_enforcers, remove_enforcers, get_enforcers)
-from Arcane.ranks import Enforcers 
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from Arcane.utils.dbfunctions import (
 add_Inspector, remove_Inspector, get_Inspector)
+from Arcane.ranks import Enforcers 
 from Arcane.ranks import Inspectors_list
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 @bot.on_message(filters.command("add", PREFIX))
 async def start(_, m: Message):
@@ -25,9 +25,9 @@ async def start(_, m: Message):
            )
 
 @bot.on_callback_query(filters.regex("ADDINS"))
-async def addinss(_, query):
-       user_id = message.reply_to_message.from_user.id
-       admin = message.from_user.id
+async def addinss(_, query, m: Message):
+       user_id = m.reply_to_message.from_user.id
+       admin = m.from_user.id
        if query.from_user.id in DEVS:
                await add_Inspector(user_id)
                text = f"""  [{admin}](tg://user?id={admin}) Promoted [{query.from_user.id}](tg://user?id={query.from_user.id}) Into Inspector"""
