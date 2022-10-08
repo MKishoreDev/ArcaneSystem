d7ccb8019a9701802f260397e6ee0b61c74fd3a1
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-08 08:48:01 +0530

    Update add.py

--- Arcane/plugins/add.py
+++ Arcane/plugins/add.py
@@ -9,7 +9,7 @@ from Arcane.utils.dbfunctions import (
 add_Inspector, remove_Inspector, get_Inspector)
 from Arcane.ranks import Enforcers 
 from Arcane.ranks import Inspectors_list
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 @bot.on_message(filters.command("add", PREFIX))
 async def start(_, m: Message):
@@ -27,8 +27,10 @@ async def start(_, m: Message):
            )
 
 @bot.on_callback_query(filters.regex("ADDINS"))
-async def addinss(_, query):
+async def addinss(_, query: CallbackQuery):
        if query.from_user.id in DEVS:
                await add_Inspector(user_id)
-               text = f"""  [{admin}](tg://user?id={admin}) Promoted [{query.from_user.id}](tg://user?id={query.from_user.id}) Into Inspector"""
-               await query.message.edit(text, reply_markup=InlineKeyboardMarkup(PROINS_BUTTON))
+               ADDIND_TEXT = f"""  [{admin}](tg://user?id={admin}) Promoted [{query.from_user.id}](tg://user?id={query.from_user.id}) Into Inspector"""
+               await query.edit_message_caption(ADDIND_TEXT, reply_markup=InlineKeyboardMarkup(INSED_BUTTON))
+
+
