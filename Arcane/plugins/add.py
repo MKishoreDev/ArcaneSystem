f88eb22789cb92b8393b207ef89674bc5fca6a22
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-10-08 11:06:51 +0530

    Update add.py

--- Arcane/plugins/add.py
+++ Arcane/plugins/add.py
@@ -36,18 +36,23 @@ async def addinss(_, query):
 @bot.on_callback_query(filters.regex("ADDENF"))
 async def addenfs(_, query):
        if query.from_user.id in DEVS:
-               await remove_Inspector(user_id)
                await add_enforcers(user_id)
-               ADDENF_TEXT = f"""[{query.from_user.id}](tg://user?id={query.from_user.id}) Successful Demoted [{user_id}](tg://user?id={user_id}) Into Enforcer"""
+               ADDENF_TEXT = f"""[{query.from_user.id}](tg://user?id={query.from_user.id}) Successful Promoted [{user_id}](tg://user?id={user_id}) Into Enforcer"""
                await query.edit_message_caption(ADDENF_TEXT, reply_markup=InlineKeyboardMarkup(ENFED_BUTTON))
 
-@bot.on_callback_query(filters.regex("DEMOTE"))
-async def demotee(_, query):
+@bot.on_callback_query(filters.regex("RMINS"))
+async def rmins(_, query):
        if query.from_user.id in DEVS:
                await remove_Inspector(user_id)
+               RMINS_TEXT = f"""[{query.from_user.id}](tg://user?id={query.from_user.id}) Successfully [{user_id}](tg://user?id={user_id}) Into civilian"""
+               await query.edit_message_caption(RMINS_TEXT, reply_markup=InlineKeyboardMarkup(CIVED_BUTTON))
+
+@bot.on_callback_query(filters.regex("RMENF"))
+async def rmenf(_, query):
+       if query.from_user.id in DEVS:
                await remove_enforcers(user_id)
-               DECIV_TEXT = f"""[{query.from_user.id}](tg://user?id={query.from_user.id}) Promoted [{user_id}](tg://user?id={user_id}) Into civilian"""
-               await query.edit_message_caption(DECIV_TEXT, reply_markup=InlineKeyboardMarkup(CIVED_BUTTON))
+               RMINS_TEXT = f"""[{query.from_user.id}](tg://user?id={query.from_user.id}) Successfully [{user_id}](tg://user?id={user_id}) Into civilian"""
+               await query.edit_message_caption(RMINS_TEXT, reply_markup=InlineKeyboardMarkup(CIVED_BUTTON))
 
 @bot.on_callback_query(filters.regex("delete"))
 async def delete(_, query):
